d8d8639f45a6d8766a6a0fa9376896424c0d96f2
Jessie Sidhu <jsidhu22@gmail.com>
2019-06-03 18:04:09 -0700

    navbar title slides component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Title from './components/Title'
+import Title from './components/Title';
+import Slides from './components/Slides';
 import './App.css';
 
 function App() {
   return (
     <>
       <Title/>
+      <Slides/>
     </>
   );
 }
--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -3,10 +3,33 @@ import Logo from './../../images/logo.png';
 
 const Title = () => {
     return (
-        <div className='row '>
-            <div className='col-md-8 offset-2 border-bottom border-dark text-center' style={{width: '400px', height: 'auto'}}>
-             <img className='p-0 m-0' src={Logo} alt='logo' style={{width: '40vw', height: 'auto'}}/>
+        <div className='row mb-2' style={{fontFamily: 'Comfortaa', fontSize: '20px'}}>
+
+            <div className='col-md-4 text-center' >
+                <img className='p-0 m-0' src={Logo} alt='logo' style={{ width: '35vw', height: 'auto' }} />
             </div>
+
+            <div className='col-md-8 '>
+
+                <div className='row align-content-md-center text-center h-100'>
+
+                    <div className="col-md-3 ">
+                        <a className=" text-dark" href="#instagram" style={{}}>Instagram</a>
+                    </div>
+                    <div className="col-md-3 ">
+                        <a className=" text-dark" href="#location" style={{}}>About Me</a>
+                    </div>
+                    <div className="col-md-3 ">
+                        <a className=" text-dark" href="#request" style={{}}>Inquire</a>
+                    </div>
+                    <div className="col-md-3 ">
+                        <a className=" text-dark" href="#request" style={{}}>FAQs</a>
+                    </div>
+
+                </div>
+
+            </div>
+
         </div>
     )
 }
