ec91dc4f29646c4fc62ea9e67b58f5162f888380
Jessie Sidhu <jsidhu22@gmail.com>
2019-06-03 18:44:36 -0700

    Updates

---
+++ precache-manifest.2208cbcb3bf1c653f7ee67f669dc2a40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "489cec6147e1cb68e26f23b9d018d168",
+    "url": "/mandeep-stylist/index.html"
+  },
+  {
+    "revision": "0eefb07f77a930b3ac04",
+    "url": "/mandeep-stylist/static/css/main.8544cd88.chunk.css"
+  },
+  {
+    "revision": "ba39d699754822bab420",
+    "url": "/mandeep-stylist/static/js/2.b44a45b8.chunk.js"
+  },
+  {
+    "revision": "0eefb07f77a930b3ac04",
+    "url": "/mandeep-stylist/static/js/main.9ebaa3e3.chunk.js"
+  },
+  {
+    "revision": "ca170f57c9b91758c088",
+    "url": "/mandeep-stylist/static/js/runtime~main.0dcf333d.js"
+  },
+  {
+    "revision": "a5c6640b1d1b8ec4fde6369503aa5365",
+    "url": "/mandeep-stylist/static/media/logo.a5c6640b.png"
+  }
+]);
