2cc619fc611ec29487887f142f5d68f27f320ab3
Jessie Sidhu <jsidhu22@gmail.com>
2019-06-27 18:09:08 -0700

    Updates

---
+++ precache-manifest.20648c29244a795ec5ea939f07e0a6e0.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa374435168ad9fcd4d88b0c9f1b925e",
+    "url": "/mandeep-stylist/index.html"
+  },
+  {
+    "revision": "530ce2713e059c855b34",
+    "url": "/mandeep-stylist/static/css/2.3ce523bb.chunk.css"
+  },
+  {
+    "revision": "197387c8c0fe27173190",
+    "url": "/mandeep-stylist/static/css/main.1f8a71bf.chunk.css"
+  },
+  {
+    "revision": "530ce2713e059c855b34",
+    "url": "/mandeep-stylist/static/js/2.2df10e7a.chunk.js"
+  },
+  {
+    "revision": "197387c8c0fe27173190",
+    "url": "/mandeep-stylist/static/js/main.cce6ee95.chunk.js"
+  },
+  {
+    "revision": "ca170f57c9b91758c088",
+    "url": "/mandeep-stylist/static/js/runtime~main.0dcf333d.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/mandeep-stylist/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/mandeep-stylist/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/mandeep-stylist/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/mandeep-stylist/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/mandeep-stylist/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/mandeep-stylist/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/mandeep-stylist/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/mandeep-stylist/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/mandeep-stylist/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/mandeep-stylist/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/mandeep-stylist/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/mandeep-stylist/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/mandeep-stylist/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/mandeep-stylist/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/mandeep-stylist/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/mandeep-stylist/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
