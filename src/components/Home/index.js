3fe333bff3bdddf2d3341c63962161c723cad721
Jessie Sidhu <jsidhu22@gmail.com>
2021-01-02 13:40:43 -0800

    Added Service/Price Table to Services Page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,17 +4,17 @@ import mshoker from '../../images/MSHOKER.jpeg';
 
 const Home = () => {
     return (
-        <div id='home-box' className='row' style={{minHeight: '100vh'}}>
-            <div id='home-content' className='col-md-12 pt-5' style={{minHeight: '75vh'}}>
+        <div id='home-box' className='row' style={{minHeight: '100vh', minWidth: '100vw'}}>
+            <div id='home-content' className='col-md-12 pt-5' style={{minHeight: '75vh', minWidth: '75vw'}}>
 
-                <div id='bio-box' className='row justify-content-md-center p-3 align-items-center'>
-                   <div className='col-md-5  '>
-                         <img className='' src={mshoker} alt='' width='80%' ></img>
+                <div id='bio-box' className='row justify-content-md-center align-items-center m-3'>
+                   <div className='col-md-4 m-2'>
+                         <img className='' src={mshoker} alt='' width='105%' ></img>
                    </div>
 
-                    <div className='col-md-7  '>
+                    <div className='col-md-7 justify-content-md-center '>
                       <div className='' >
-                        <p id='bio' className='text-justify' style={{fontSize: '1.5em', color: 'white'}}> Mandeep [Mun•deep] has been in hair industry since ‘07. Her journey started in Portland, Oregon and cruised its way to New York City where she gained experience with all of the beautiful forms of hair; straight to textured. She specializes in everything from Color-Corrections to Keratins and has very well perfected her cutting styles. Mandeep is easy going, well educated in her craft, and stays up to date with all the new trends so she can best serve all her clients. Her energetic corky personality is well loved, she believes in honest work and considers herself a perfectionist. After collectively working at many high-end salons; Patrick Evan Salon of San Francisco being the last, Mandeep is confident she can be the perfect stylist for you.</p>
+                        <p id='bio' className='text-justify' style={{fontSize: '1.6em', color: 'white'}}> Mandeep [Mun•deep] has been in hair industry since ‘07. Her journey started in Portland, Oregon and cruised its way to New York City where she gained experience with all of the beautiful forms of hair; straight to textured. She specializes in everything from Color-Corrections to Keratins and has very well perfected her cutting styles. Mandeep is easy going, well educated in her craft, and stays up to date with all the new trends so she can best serve all her clients. Her energetic corky personality is well loved, she believes in honest work and considers herself a perfectionist. After collectively working at many high-end salons; Patrick Evan Salon of San Francisco being the last, Mandeep is confident she can be the perfect stylist for you.</p>
                       </div>  
                     </div>
                 </div>
--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -3,11 +3,146 @@ import './services.css';
 
 const Services = () => {
     return (
-        <div id='services-box' className='' style={{minHeight: '100vh'}}>
-            <div id='services-content' style={{minHeight: '100vh'}}>
-                
-            </div>
+        <div id='services-box' className='row pt-5' style={{minHeight: '100vh', minWidth: '100vw'}}>
+            <div id='services-content' className='col-md-12 mt-1 mb-5 pt-3 ' style={{minHeight: '100vh', minWidth: '75vw'}}>
+                <div className='row justify-content-sm-center m-3'>
+                    <table className='col-md-7 '>
+                        <p className='text-justify pr-2' style={{fontSize: '1.1em', fontWeight: 'bold'}}>* Prices vary depending on your Hair Length and Texture. Please Contact Mandeep for an exact price on your length/type of hair. Some services may require an in-person consultation.</p>
 
+                        <div className=" text-left ">
+                          <thead>
+                            <tr>
+                              <th scope="col "></th>
+                              <th scope="col ">Services</th>
+                              <th scope="col ">Pricing</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                              {/* Styling */}
+                            <tr>
+                              <th>Styling</th>
+                              <td>BlowOut</td>
+                              <td>$75</td>
+                            </tr>
+                            <tr>
+                              <th></th>
+                              <td>Treatment + BlowOut</td>
+                              <td></td>
+                            </tr>
+                            <tr>
+                              <th></th>
+                              <td>Gloss + BlowOut</td>
+                              <td></td>
+                            </tr>
+                            <tr>
+                              <th></th>
+                              <td>Updo</td>
+                              <td></td>
+                            </tr>
+                            <tr>
+                              <th></th>
+                              <td>Braid&#40;s&#41;</td>
+                              <td></td>
+                            </tr>
+                              {/* Cuts */}
+                            <tr>
+                              <th>Cuts</th>
+                              <td>Hair Cut</td>
+                              <td></td>
+                            </tr>
+                            <tr>
+                              <th></th>
+                              <td>Precision Cut</td>
+                              <td></td>
+                            </tr>
+                            <tr>
+                              <th></th>
+                              <td>Dead End Treament</td>
+                              <td></td>
+                            </tr>
+                            <tr>
+                              <th></th>
+                              <td>Bang Trim</td>
+                              <td></td>
+                            </tr>
+                              {/* Color */}
+                            <tr>
+                              <th>Color</th>
+                              <td>All Over Tint</td>
+                              <td></td>
+                            </tr>
+                            <tr>
+                              <th></th>
+                              <td>Tint &#40;Root touchup&#41;</td>
+                              <td></td>
+                            </tr>
+                            <tr>
+                              <th></th>
+                              <td>Full Highlight</td>
+                              <td></td>
+                            </tr>
+                            <tr>
+                              <th></th>
+                              <td>Partial Highlight</td>
+                              <td></td>
+                            </tr>
+                            <tr>
+                              <th></th>
+                              <td>Toner &#40;Color Gloss&#41;</td>
+                              <td></td>
+                            </tr>
+                            <tr>
+                              <th></th>
+                              <td>Balage</td>
+                              <td></td>
+                            </tr>
+                            <tr>
+                              <th></th>
+                              <td>Foilage</td>
+                              <td></td>
+                            </tr>
+                            <tr>
+                              <th></th>
+                              <td>Color Correction</td>
+                              <td></td>
+                            </tr>
+                            <tr>
+                              <th></th>
+                              <td>Creative Color</td>
+                              <td></td>
+                            </tr>
+                            <tr>
+                              <th></th>
+                              <td>Bleach + Tone</td>
+                              <td></td>
+                            </tr>
+                              {/* Hair Texture */}
+                            <tr>
+                              <th>Hair Texture</th>
+                              <td>Keratin Express/Brazilian BlowOut<br></br>&#40;Formaldehyde Free&#41;</td>
+                              <td></td>
+                            </tr>
+                            <tr>
+                              <th></th>
+                              <td>Keratin/Brazilian Treatment</td>
+                              <td></td>
+                            </tr>
+                              {/* Hair Extension */}
+                            <tr>
+                              <th>Hair Extensions</th>
+                              <td>Microlinks - New Set</td>
+                              <td></td>
+                            </tr>
+                            <tr>
+                              <th></th>
+                              <td>Microlinks Push-up</td>
+                              <td></td>
+                            </tr>
+                          </tbody>
+                        </div>
+                    </table>
+                </div>
+            </div>
         </div>
     )
 }
